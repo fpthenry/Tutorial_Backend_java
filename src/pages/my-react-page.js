383fcb4053d606336944049214bc3337055aed7a
fpthenry <fpthenry@gmail.com>
2023-09-02 17:24:05 +0700

    delete

--- src/pages/my-react-page.js
+++ src/pages/my-react-page.js
@@ -4,7 +4,7 @@ import Layout from '@theme/Layout';
 export default function MyReactPage() {
   return (
     <Layout>
-      <h1>My React page edit by nguyen anh tua</h1>
+      <h1>My React page</h1>
       <p>This is a React page</p>
     </Layout>
   );
